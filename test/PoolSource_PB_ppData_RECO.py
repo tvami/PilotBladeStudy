8dc693db43e3b040430d34ccefd3f2a9be2153ac
Tamas Vami <vami.tamas@wigner.mta.hu>
2016-03-08 09:24:25 +0100

    Delete PoolSource_PB_ppData_RECO.py

--- test/PoolSource_PB_ppData_RECO.py
+++
@@ -1,9 +0,0 @@
-# Desciption: example how to set up many input files. After 255 files put the next files to the .extend part
-import FWCore.ParameterSet.Config as cms
-myfilelist = cms.untracked.vstring()
-myfilelist.extend([
-  'file:/data/vami/projects/pilotBlade/ppProcessing/Results/PilotMinBias_13TeV_cfi_py_RAW2DIGI_L1Reco_RECO2900Event.root',
-  ])
-#myfilelist.extend([  
-#  ])
-FileNames = myfilelist
