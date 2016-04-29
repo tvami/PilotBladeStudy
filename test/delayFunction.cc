0fcf8188cfc6bf1e804197ee4bf47707825db2ef
Tamas Vami <vami.tamas@wigner.mta.hu>
2016-04-29 18:19:05 +0200

    Update delayFunction.cc

--- test/delayFunction.cc
+++ test/delayFunction.cc
@@ -101,9 +101,14 @@ int delay(uint run, const uint ls) {
     if (ls<28)  { delay=96; return delay; }
     if (ls<38)  { delay=98; return delay; }
     if (ls<48)  { delay=100; return delay;}
-        if (ls<48)  { delay=100; return delay;}
-            if (ls<48)  { delay=100; return delay;}
-    else       { delay=96; return delay; }
+    if (ls<58)  { delay=102; return delay;}
+    if (ls<69)  { delay=104; return delay;}
+    if (ls<80)  { delay=106; return delay;}
+    if (ls<90)  { delay=108; return delay;}
+    if (ls<100)  { delay=110; return delay;}
+    if (ls<110)  { delay=112; return delay;}
+    if (ls<122)  { delay=114; return delay;}
+    else        { delay=62; return delay; }
   }
   return delay;
 }
