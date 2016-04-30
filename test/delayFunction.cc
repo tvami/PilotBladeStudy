67ce3791a60a32c1a9129ba02d0169465fe31753
Tamas Vami <vami.tamas@wigner.mta.hu>
2016-04-30 15:24:19 +0200

    Update delayFunction.cc

--- test/delayFunction.cc
+++ test/delayFunction.cc
@@ -86,11 +86,9 @@ int delay(uint run, const uint ls) {
     if (ls<12) { delay=52; return delay; }
     else       { delay=49; return delay; }
   }
-  if (run==272008 || run==272009 || run==272010 || run==272011) {
+  // 2016-04-29 from here
+  if (run==272008 || run==272010 || run==272011 || run==272012 || run==272014) {
     delay=90; return delay;
-  }
-  if (run==272012 || run==272013 || run==272014) {
-    delay=92; return delay;
   }
   if (run==272021) {
     if (ls<6)   { delay=92; return delay;}
