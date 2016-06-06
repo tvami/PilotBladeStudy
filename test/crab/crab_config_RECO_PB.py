ef57cd5207e6543ce2e018d7e6ece607d8363b11
Tamas Vami <vami.tamas@wigner.mta.hu>
2016-06-06 11:26:31 +0200

    Update crab_config_RECO_PB.py

--- test/crab/crab_config_RECO_PB.py
+++ test/crab/crab_config_RECO_PB.py
@@ -2,7 +2,7 @@ from CRABClient.UserUtilities import config
 config = config()
 
 config.section_('General')
-config.General.requestName = 'PilotBlade_data_RECO_pp_Run274387_v1'
+config.General.requestName = 'PilotBlade_data_RECO_pp_Run274000To274443_v1'
 config.General.transferOutputs = True
 config.General.transferLogs = True
 
@@ -17,7 +17,7 @@ config.Data.inputDataset = '/ExpressPhysics/Run2016B-Express-v2/FEVT'
 config.Data.runRange = '274000-274443'
 config.Data.splitting = 'LumiBased'
 config.Data.unitsPerJob = 1 
-config.Data.outLFNDirBase = '/store/user/tvami/PilotBlade_data_RECO_pp_Run274387_v1/'
+config.Data.outLFNDirBase = '/store/user/tvami/PilotBlade_data_RECO_pp_Run274000To274443_v1/'
 config.Data.publication = True
 config.Data.outputDatasetTag  = config.General.requestName
 
