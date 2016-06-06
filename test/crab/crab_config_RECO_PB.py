4ed90f535d86ef7ad2034a609403051e1216277f
Tamas Vami <vami.tamas@wigner.mta.hu>
2016-06-06 10:31:11 +0200

    Add files via upload

---
+++ test/crab/crab_config_RECO_PB.py
@@ -0,0 +1,25 @@
+from CRABClient.UserUtilities import config 
+config = config()
+
+config.section_('General')
+config.General.requestName = 'PilotBlade_data_RECO_pp_Run271056and271084_v1'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+config.section_('JobType')
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = '2ppRaw2Reco_CMSSW80X.py'
+#config.JobType.maxMemoryMB = 3000
+
+
+config.section_('Data')
+config.Data.inputDataset = '/ExpressPhysics/Run2016A-Express-v1/FEVT'
+config.Data.runRange = '273728'
+config.Data.splitting = 'LumiBased'
+config.Data.unitsPerJob = 1 
+config.Data.outLFNDirBase = '/store/user/tvami/PilotBlade_data_RECO_pp_Run273728_v1/'
+config.Data.publication = True
+config.Data.outputDatasetTag  = config.General.requestName
+
+config.section_('Site')
+config.Site.storageSite = 'T2_HU_Budapest'
