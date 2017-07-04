0c7a4cca7fc73b2b42a8e451c6613460bfe8f15d
Tamas Vami <vami.tamas@wigner.mta.hu>
2017-07-04 20:56:51 +0200

    Add files via upload

---
+++ other/0plottingScript.sh
@@ -0,0 +1,21 @@
+#mkdir digis
+#mkdir clusters
+mkdir rechits
+cd /data/vami/projects/pilotBlade/uTCA-data/pp-ZeroBias/CMSSW_8_0_20_patch1/src
+cmsenv
+cd -
+make clean
+make -Wsign-compare
+./PilotHistoMaker
+cd digis/
+mkdir Cs
+mv *.C Cs/.
+cd ..
+cd clusters/
+mkdir Cs
+mv *.C Cs/.
+cd ..
+cd rechits/
+mkdir Cs
+mv *.C Cs/.
+cd ..
---
+++ other/0wgetScript.sh
@@ -0,0 +1,5 @@
+wget https://raw.githubusercontent.com/tvami/cmssw80X-PilotBlade/PilotBladeStudy/DPGAnalysis/PilotBladeStudy/other/Makefile
+wget https://raw.githubusercontent.com/tvami/cmssw80X-PilotBlade/PilotBladeStudy/DPGAnalysis/PilotBladeStudy/other/Makefile.arch
+wget https://raw.githubusercontent.com/tvami/cmssw80X-PilotBlade/PilotBladeStudy/DPGAnalysis/PilotBladeStudy/other/PilotHistoMaker.C
+wget https://raw.githubusercontent.com/tvami/cmssw80X-PilotBlade/PilotBladeStudy/DPGAnalysis/PilotBladeStudy/other/PilotHistoMaker.h
+wget https://raw.githubusercontent.com/tvami/cmssw80X-PilotBlade/PilotBladeStudy/DPGAnalysis/PilotBladeStudy/other/0plottingScript.sh
